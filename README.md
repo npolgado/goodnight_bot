3c97687457fd5f82be4d003112a9386f6117a74d
nicholas <nicholas831@gmail.com>
2023-11-26 01:53:26 -0800

    sweet dreams silent boi

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # goodnight_bot
 discord goodnight bot to reply to goodnight messages in #general
+
+
+# main.py
+
+contains python script for the bot. the main functionality is listening for "goodnight" or "gn" messages, liking them, and responding with "Goodnight :)"
+
+this needs to be run in the background somewhere in order for the bot to work. 
+
+
+# To Install
+
+I followed this guide to make a bot
+
+[Creating an application using discord developer portal](https://discord.com/developers/applications)
+
+[Read Discord chats with Python](https://blog.tinq.ai/read-discord-chats-with-python/)
+
+[Client parameters in python](https://stackoverflow.com/questions/71959420/client-init-missing-1-required-keyword-only-argument-intents-or-tak)
--- main.py
+++ main.py
@@ -1,35 +1,64 @@
 import os
 import requests
 import discord
+from discord.ext import commands
 import re
 import os
+from datetime import datetime
 
-API_TOKEN = os.environ['API_TOKEN']
+GOODNIGHT_TIMES = [20, 21, 22, 23, 0, 1, 2, 3, 4]
 
-client = discord.Client(intents=discord.Intents.all())
+API_TOKEN = 'MTE3MTM1OTM2MzE3MzIwODA3NA.GCkaYm.At_8JyON_HOIqYOTBObjoPgqeAGzdlv5Bi6QT8'# os.environ['API_TOKEN']
+
+intents = discord.Intents.all()
+intents.voice_states = True
+
+client = discord.Client(intents=intents)
 guild = discord.Guild
 
 pattern = re.compile(r'\bg(?:ood)?\s?n(?:ight)?\b', re.IGNORECASE)
 
+goodnight_emoji = ":regional_indicator_g: :regional_indicator_o: :regional_indicator_o: :regional_indicator_d: :regional_indicator_n: :regional_indicator_i: :regional_indicator_g: :regional_indicator_h: :regional_indicator_t:"
+
+
+def is_goodnight_time():
+    current_hour = datetime.now().hour
+    return current_hour in GOODNIGHT_TIMES
+
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game('_scan help'))
+    await client.change_presence(activity=discord.Game('waiting to goodnight :)'))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+
+    if is_goodnight_time():
+        if pattern.search(message.content) is not None:
+            print("[BOT] Found goodnight message")
+            for i in goodnight_emoji.split(" "):
+                await message.add_reaction(i)
+            await message.reply('Goodnight :)')
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if member == client.user:
+        return
     
-    message_content = message.content
-    message_author = message.author
-
-    print(message_author)
-    # print(message_content)
-
-    if pattern.search(message_content) is not None:
-        print("[BOT] Found goodnight message")
-        await message.add_reaction('👍')
-        await message.reply('Goodnight :)')
+    if before.channel and not after.channel:
+        # User disconnected from a voice channel
+        print(f'{member.name} disconnected from {before.channel.name}')
+        
+        if is_goodnight_time():
+            if "knoble" in member.name:
+                # spam goodnight
+                for i in range(2):
+                    mention = member.mention
+                    await client.get_channel(1035445680786911283).send('Goodnight Knoble :)')  
+            else:
+                mention = member.mention
+                await client.get_channel(1035445680786911283).send(f'Goodnight {mention} :)')
 
 if __name__ == "__main__":
     client.run(API_TOKEN)

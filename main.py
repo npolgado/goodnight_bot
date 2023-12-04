46aab66ff79bf7de36eb0f57814dd94ffe4711a7
nicholas <nicholas831@gmail.com>
2023-12-04 03:40:34 -0800

    debacle task

--- main.py
+++ main.py
@@ -10,7 +10,30 @@ from datetime import datetime
 import time
 
 GOODNIGHT_TIMES = [22, 23, 0, 1, 2]
-SHIDR_HOURS = [3, 4, 5, 6]
+REAL_LATE_HOURS = [3, 4, 5, 6]
+
+REAL_LATE_QUIPS = [
+    'its really quite simple.. just sleep',
+    'this is shitter hours.. why are you awake?',
+    'its past 2am and you think this is a good idea?',
+    'why do i have to play mommy and tell you to go to bed?',
+    'its time to sleep... or is it?',
+    'OHHH???',
+    'really...?',
+    '?????',
+    'its gettttinnn late',
+    'aight bro I get it, you are a night owl',
+    'im actually tilted, just dc!',
+    'im livid',
+    'im actually so mad',
+    'just say that you hate me and go',
+    'aight im done',
+    'aight im done',
+    'youll never hear from me again',
+    'fuming atm',
+    'curse words are allowed in this server at this moment btw... just saying',
+    '>:('
+]
 
 GOODNIGHT_QUIPS = [
     'Mwah!',
@@ -23,7 +46,25 @@ GOODNIGHT_QUIPS = [
     'goodnight to all the sleepy people',
     'zzzzzzz',
     'Mwah Mwah Mwah',
-    'golly gee its late',           
+    'golly gee its late',
+    'its time to sleep... or is it?',
+    'gee wilikers im getting sleepy',
+    'heavens to betsy im getting ',
+    'Dream big, sleep well!',
+    'Time to recharge those batteries!',
+    'May your dreams be as sweet as sugar!',
+    'Off to the land of nod!',
+    'Wishing you a peaceful night and sweet dreams!',
+    'As the stars twinkle, its time to get some sprinkle of dreams!',
+    'Rest those eyes, tomorrow is a new sunrise!',
+    'Goodnight, sleep like a baby!',
+    'Sending you a pillow of happy thoughts!',
+    'May your sleep be as deep as your dreams!',
+    'Night is here, the moon is bright, and its time to say goodnight!',
+    'Sleep is the best meditation. Goodnight!',
+    'Dreamland is calling, are you ready to answer?',
+    'Close your eyes and imagine the impossible. Goodnight!',
+    'Sleep is the golden chain that ties health and our bodies together.'
 ]
 
 API_TOKEN = os.environ['API_TOKEN']
@@ -39,7 +80,7 @@ guild = discord.Guild
 
 pattern = re.compile(r'\bg(?:ood)?\s?n(?:ight)?\b', re.IGNORECASE)
 
-def is_shiddr_hour():
+def is_real_late_hour():
     current_hour = datetime.now().hour
     return current_hour in SHIDR_HOURS
 
@@ -51,6 +92,7 @@ def is_goodnight_time():
 async def on_ready():
     print("[BOT] Changing precense...")
     await client.change_presence(activity=discord.Game('waiting to goodnight :)'))
+    await sweet_nothings.start()
 
 @client.event
 async def on_message(message):
@@ -59,7 +101,7 @@ async def on_message(message):
 
     print(f"[BOT] recieved message: {message.content}")
 
-    if is_goodnight_time():
+    if is_goodnight_time() or is_real_late_hour():
         if pattern.search(message.content) is not None:
             print("[BOT] Found goodnight message")
             await message.add_reaction('👍')
@@ -72,18 +114,17 @@ async def on_voice_state_update(member, before, after):
     
     if before.channel is None and after.channel is not None:
         channel = after.channel
-        if is_shiddr_hour():
+        
+        if is_real_late_hour():
             print(f'[BOT] {member.name} joined {channel.name} during a shiddr hour')
             await client.get_channel(goobs_lounge_general).send(f'whoa whoa.. good, MoRnInG {member.mention} >:(')
-            await client.get_channel(goobs_lounge_general).send('dawg its really quite simple.. just sleep')
-            await client.get_channel(goobs_lounge_general).send('this is shitter hours.. why are you awake?')
+            await real_late_debacle.start()
 
     if before.channel and not after.channel:
-        print(f'[BOT] {member.name} disconnected from {before.channel.name}')
         mention = member.mention
 
         if is_goodnight_time():
-
+            print(f'[BOT] {member.name} disconnected from {before.channel.name} in a goodnight hour')
             if "knoble" in member.name: # knoble clause
                 for i in range(2):
                     await client.get_channel(goobs_lounge_general).send('Goodnight Knoble :)')
@@ -106,6 +147,18 @@ async def on_raw_reaction_add(payload):
         # Respond with the same emoji
         await message.add_reaction('💤')
 
+# @client.command(name='pause')
+# async def pause(ctx):
+#     global is_paused
+#     is_paused = True
+#     await ctx.send("Bot has been paused. Use `!resume` to resume.")
+
+# @client.command(name='resume')
+# async def resume(ctx):
+#     global is_paused
+#     is_paused = False
+#     await ctx.send("Bot has been resumed.")
+
 @tasks.loop(minutes=30)
 async def sweet_nothings():
     print("[BOT] Checking if its time to send a sweet nothing")
@@ -114,6 +167,16 @@ async def sweet_nothings():
     if channel and is_goodnight_time():
         print("[BOT] Sending sweet nothing")
         selected_message = random.choice(GOODNIGHT_QUIPS)
+        await channel.send(selected_message)
+
+@tasks.loop(seconds=1, count=3)
+async def real_late_debacle():
+    print("[BOT] has been triggered to send a real late debacle")
+    channel = client.get_channel(goobs_lounge_general)
+    
+    if channel and is_real_late_hour():
+        print("[BOT] Sending choice debacle message")
+        selected_message = random.choice(REAL_LATE_QUIPS)
         await channel.send(selected_message)
 
 if __name__ == "__main__":

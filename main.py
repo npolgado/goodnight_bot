7f828084c64695aed8b51927aa68a3208a859df0
nicholas <nicholas831@gmail.com>
2024-01-18 18:31:51 -0800

    v1.5.1 display yesterdays rare good night chance

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import time
 import sys
 
-VERSION = "1.5.0"
+VERSION = "1.5.1"
 
 GOODNIGHT_TIMES = [22, 23, 0, 1, 2]
 REAL_LATE_HOURS = [2, 3, 4, 5] # EYES EMOJI
@@ -41,42 +41,36 @@ REAL_LATE_QUIPS = [
 ]
 
 GOODNIGHT_QUIPS = [
-    'Mwah!',
+    'Mwah! <3',
     'its gettttinnn late *SIGGGGGGGGH* Sleep Tight!',
     'goodnight to anyone who is still awake but possibly sleepy',
     'ah that time of the night huh',
     'wakey wakey?? more like sleepey sleepey',
     'goodnight to all the sleepy people',
-    'zzzzzzz',
-    'Mwah Mwah Mwah',
-    'golly gee its late',
+    'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
+    'golly gee its late... Mwah Mwah Mwah',
     'its time to sleep... or is it?',
     'gee wilikers im getting sleepy',
     'heavens to betsy im getting real real real REAAAAAAL tired!1!',
     'Dream big, sleep well!',
     'Time to recharge those batteries!',
     'May your dreams be as sweet as sugar!',
-    'Off to the land of nod!',
     'Wishing you a peaceful night and sweet dreams!',
     'As the stars twinkle, its time to get some sprinkle of dreams!',
-    'Rest those eyes, tomorrow is a new sunrise!',
+    'Rest those eyes, tomorrow is a new sunrise! Off to the land of nod!',
     'Goodnight, sleep like a baby! Sending you a pillow of happy thoughts!',
     'May your sleep be as deep as your dreams!',
-    'Night is here, the moon is bright, and its time to say goodnight!',
-    'Sleep is the best meditation. Goodnight!',
+    'Night is here, the moon is bright, and its time to say goodnight! Sleep is the best meditation!',
     'Dreamland is calling, are you ready to answer?',
-    'Close your eyes and imagine the impossible. Goodnight!',
-    'Sleep is the golden chain that ties health and our bodies together.',
+    'Close your eyes and imagine the impossible. Goodnight! Sleep is the golden chain that ties health and our bodies together.',
     'can HARDLY KEEP MY EYES OPEN... SHEEEEEEEESH',
     'sleep now so I can blow you a kiss',
     'Alllllright later bro, goodnight dude, aight bro im out of here... alright goodnight',
     'Aight bro, its getting late, peace dude cya dude, goodnight bro, alright bro im out of here... alright goodnight',
-    'if you dont respond in 5 SECONDS.....',
     'sooo RAM, Scav, jay, ep? jay ep ram ram ram?? ep ep ep! scav scav scav! ram jam ram jam unless remake/dog',
     'sooo ketketketketketketketket jay ep ram ram ram?? gotta ep patch then patch then ram?? YOU CAN NEVER THANK ME ENOUGH',
     'aight bro, im out of here... alright goodnight',
     'usually id say goodnight here but... im not going to',
-    'zzzzzzz ZEEEEE',
     'did you know, that if you dont sleep, you will die?',
     'i heard if you dont sleep in 5 seconds...',
     'sleepimon... I CHOOSE YOU!',
@@ -104,8 +98,32 @@ GOODNIGHT_QUIPS = [
     '1 in every 2,000 adults has narcolepsy. In the U.S., that equates to about 165,950 people. The record for the longest period without sleep is 18 days, 21 hours, 40 minutes during a rocking chair marathon.',
     'Think you can sleep 10 hours one night to catch up for a few nights of less than the recommended 7-9 hours of sleep? Think again. A Harvard study showed that sleeping extra to compensate for sleep times decreased reaction times and the ability to focus. That means—practice good sleep hygiene as best you can to show up as your best self every day.',
 
-    # goodnight copy pastas 
-    'Have you ever been lying in bed unable to sleep and you try to clear your mind but by doing that youve inadvertently started consciously thinking about clearing your mind and that creates a perpetual cycle then you think about going to the bathroom because you need to but then you think about how cold it is outside and suddenly you dont need to as badly anymore and then you started to feel parched and then think about how far youd have to walk to get water and then you figure that youre not about to sleep anytime soon anyway so youre just like yknow what Ill go do what i need and when Im back Ill be tired enough to instantly sleep and so you do that and then you realize its pitch black but since your eyes already adjusted you dont want to turn on the lights and you think you heard a noise and then you get really anxious and you run back to your bed but along the way you stub your toe on the textbook you accidentally left on the floor earlier and it burns like the fiery brimstones of hell and you jump up in down in silent agony since everyone else in the house is already asleep and then you get back in bed and try to ignore how dirty your feet got during the walk and you get really uncomfortable and then you start to thinking about the noise again and you start sweating and all of the sudden the beds suddenly too hot and you flip the pillow and its like really cool and nice and you close your eyes and try to sleep but you still cant and then you try to clear your mind but by doing that youve inadvertently started consciously thinking about clearing your mind and that creates a perpetual cycle where you do it all over again?'
+    # goodnight copy pastas
+    'Oh hey. Im just about to go to bed. I know we couldnt Skype tonight, but thats alright. Goodnight, girl, Ill see you tomorrow.',
+    'Oh, Greetings. Until I saw your presence, I was in the midst of exiting this current room, in order to arrive in bedroom so that I could rest. \
+        I acknowledge that we did not use the Skype app to talk to eachother through means of wireless connection, but, I think that the fact that we \
+        did not do this does not particularly matter. I hope that the rest of your night continues well, I shall be with you again tommorow.',
+    
+    'Have you ever been lying in bed unable to sleep and you try to clear your mind but by doing that youve inadvertently started consciously thinking \
+        about clearing your mind and that creates a perpetual cycle then you think about going to the bathroom because you need to but then you think  \
+        about how cold it is outside and suddenly you dont need to as badly anymore and then you started to feel parched and then think about how far  \
+        youd have to walk to get water and then you figure that youre not about to sleep anytime soon anyway so youre just like yknow what Ill go do what \
+        i need and when Im back Ill be tired enough to instantly sleep and so you do that and then you realize its pitch black but since your eyes already \
+        adjusted you dont want to turn on the lights and you think you heard a noise and then you get really anxious and you run back to your bed but along  \
+        the way you stub your toe on the textbook you accidentally left on the floor earlier and it burns like the fiery brimstones of hell and you jump up \
+        in down in silent agony since everyone else in the house is already asleep and then you get back in bed and try to ignore how dirty your feet got \
+        during the walk and you get really uncomfortable and then you start to thinking about the noise again and you start sweating and all of the sudden \
+        the beds suddenly too hot and you flip the pillow and its like really cool and nice and you close your eyes and try to sleep but you still cant and \
+        then you try to clear your mind but by doing that youve inadvertently started consciously thinking about clearing your mind and that creates a perpetual \
+        cycle where you do it all over again?',
+    
+    'Goodnight future wife hope u have sweet dreams of our future where u fall in love w me pls when u wake up tell me how buff I got ... I wanna know ;) Im sure \
+        u will still b mrs pengting that u r ... but not as sexc as me. Oh yh also pls tell me what house we live in and is my pipi game gd in ur dreams. jokes Ik \
+        the answer to that I was just fishing for compliments (btw dont have wet dreams for it changing bedsheets must be a nightmare in the middle of the night) \
+        anyways hope u sleep well ... if ur dreaming of me in the future Ik u will sleep well cuz imma b HELLA rich and xtra as u will b too ;) hope u fell better \
+        in the morning hope u have a great day at school (pls try focusing in lessons, dont have to many flash forwards of my sexc self in the future no matter how \
+        hot I am. Dont stresss to much it will b alright in the end :) as u will see in ur dreams ;) dream guy ( sexc me in future) dream job (forensic phsychologist) \
+        and very wealthy w a big house and lots of love and loving if u know what I mean..... go to sleep soon and talk to me tmrw Frey goodnight'
 ]
 
 try:
@@ -136,7 +154,7 @@ pattern = re.compile(r'\bg(?:ood)(?:\s?n(?:ight|ite)?)\b|\b(?:g(?:\s*)n)+\b', re
 goobs_lounge_general = 1035445680786911283
 goodnight_channel = 1190584590625165364
 
-todays_rare_gn_chance = 0.0
+todays_rare_gn_chance = random.uniform(RARE_GN_CHANCE_MIN, RARE_GN_CHANCE_MAX)
 rare_goodnight_has_not_been_set = True
 
 user_activity = {}
@@ -169,6 +187,15 @@ def is_goodnight_time():
     global todays_rare_gn_chance, rare_goodnight_has_not_been_set, user_activity
     # reset rare goodnight chance on first hour of the day
     if hour == 0 and rare_goodnight_has_not_been_set:
+        # send yesterdays rare goodnight chance in the goodnight channel 
+        try:
+            yesterdays_chance = todays_rare_gn_chance*100
+            client.get_channel(goodnight_channel).send(f'Yesterdays rare goodnight chance was {yesterdays_chance} % :>)')
+        except Exception as e:
+            pp(f"\tCould not send yesterdays rare goodnight :()\n")
+            pp(e, True)
+
+        # reset todays rare goodnight chance
         todays_rare_gn_chance = random.uniform(RARE_GN_CHANCE_MIN, RARE_GN_CHANCE_MAX)
         rare_goodnight_has_not_been_set = False
         

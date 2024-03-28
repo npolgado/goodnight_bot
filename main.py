1b865483742af8e591752672ad6ce457a42c1332
nicholas <nicholas831@gmail.com>
2024-03-27 22:18:46 -0700

    v2.0!!! finally found real late night hours hotfix. refined quips

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import time
 import sys
 
-VERSION = "1.5.4"
+VERSION = "2.0"
 
 GOODNIGHT_TIMES = [22, 23, 0, 1, 2]
 REAL_LATE_HOURS = [2, 3, 4, 5] # EYES EMOJI
@@ -37,43 +37,37 @@ REAL_LATE_QUIPS = [
     'in this economy?, on my fucking birthday??!?!?',
     'you disgust me',
     'so schnee bro',
-    'youre so weak oml'
+    'youre so weak oml',
+    'mmmmmm good morning :))) sleep well babe? You look like an angel, do you want pancakes for breakfast? Yeah, lets walk down to the farmers market to pick up some strawber-🔔🚨🔔🚨🔔\n\tOH YOU FEELING SLEEPY PUSSY? FUCK NAH, GET THE FUCK UP. GET YOUR FUCKIN CLOTHES ON PLEDGE GET THE FUCK OUTTA BED AND INTO THAT FUCKIN PARKADE FUCKIN MOVE PUSSIES LETS GO LETS GO YOU STUPID FUCKIN PLEDGES LETS GO LETS GO LETS GO'
 ]
 
 GOODNIGHT_QUIPS = [
-    'Mwah! <3',
     'its gettttinnn late *SIGGGGGGGGH* Sleep Tight!',
     'goodnight to anyone who is still awake but possibly sleepy',
-    'ah that time of the night huh',
-    'wakey wakey?? more like sleepey sleepey',
+    'ah that time of the night huh. aight bro, im out of here... alright goodnight',
     'goodnight to all the sleepy people',
     'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
     'golly gee its late... Mwah Mwah Mwah',
-    'its time to sleep... or is it?',
-    'gee wilikers im getting sleepy',
+    'gee wilikers im getting sleepy. its time to sleep... or is it?',
     'heavens to betsy im getting real real real REAAAAAAL tired!1!',
-    'Dream big, sleep well!',
     'Time to recharge those batteries!',
-    'May your dreams be as sweet as sugar!',
+    'May your dreams be as sweet as sugar! ..... reallly.... not gonna say goodnight back.... REALLLY???!??',
     'Wishing you a peaceful night and sweet dreams!',
     'As the stars twinkle, its time to get some sprinkle of dreams!',
     'Rest those eyes, tomorrow is a new sunrise! Off to the land of nod!',
     'Goodnight, sleep like a baby! Sending you a pillow of happy thoughts!',
-    'May your sleep be as deep as your dreams!',
     'Night is here, the moon is bright, and its time to say goodnight! Sleep is the best meditation!',
     'Dreamland is calling, are you ready to answer?',
     'Close your eyes and imagine the impossible. Goodnight! Sleep is the golden chain that ties health and our bodies together.',
     'can HARDLY KEEP MY EYES OPEN... SHEEEEEEEESH',
-    'sleep now so I can blow you a kiss',
+    'sleep now so I can blow you a kiss. Mwah! <3',
     'Alllllright later bro, goodnight dude, aight bro im out of here... alright goodnight',
     'Aight bro, its getting late, peace dude cya dude, goodnight bro, alright bro im out of here... alright goodnight',
     'sooo RAM, Scav, jay, ep? jay ep ram ram ram?? ep ep ep! scav scav scav! ram jam ram jam unless remake/dog',
     'sooo ketketketketketketketket jay ep ram ram ram?? gotta ep patch then patch then ram?? YOU CAN NEVER THANK ME ENOUGH',
-    'aight bro, im out of here... alright goodnight',
     'usually id say goodnight here but... im not going to',
     'did you know, that if you dont sleep, you will die?',
     'i heard if you dont sleep in 5 seconds...',
-    'sleepimon... I CHOOSE YOU!',
     'have a swell night night :)',
     'why keep eye open when eye close do trick!!',
     'alllright bro, im out of here... alright goodnight',
@@ -83,32 +77,28 @@ GOODNIGHT_QUIPS = [
     'LADY LUUUUCKY SMILIN... the fuck queue up or sleep the fuck',
     'NOW IM NOT GONNA SSAAAAAAYYYYYYY GO TO SLEEP... but gtf to sleep',
     'WOWZERS THEY NOT SLEEP YET???',
-    '..... reallly.... not gonna say goodnight back.... REALLLY???!??',
-    
-    # goodnight in the theme of a pirate
-    'Yarrr, goodnight to ye matey',
-    'AHOY! goodnight to ye matey',
-    'THAR SHE BLOWWWSSS, wishing thee a goodnight',
-    'MATEY! ME GOT SCRUVY, GN GN'
-
-    # facts about sleep
-    'Sleep is important for various aspects of brain function. This includes cognition, concentration, productivity and performance.',
-    'Getting the right amount of sleep has been linked to a longer lifespan. Its normal to fall asleep between 10-20 minutes after going to bed.',
-    'Humans usually have between four to six dreams a night. Many sleep scientists believe dreams help your brain process the events of the day, so dreams are incredibly important!',
-    '1 in every 2,000 adults has narcolepsy. In the U.S., that equates to about 165,950 people. The record for the longest period without sleep is 18 days, 21 hours, 40 minutes during a rocking chair marathon.',
-    'Think you can sleep 10 hours one night to catch up for a few nights of less than the recommended 7-9 hours of sleep? Think again. A Harvard study showed that sleeping extra to compensate for sleep times decreased reaction times and the ability to focus. That means—practice good sleep hygiene as best you can to show up as your best self every day.',
 
     # goodnight copy pastas
     'Oh hey. Im just about to go to bed. I know we couldnt Skype tonight, but thats alright. Goodnight, girl, Ill see you tomorrow.',
     'Oh, Greetings. Until I saw your presence, I was in the midst of exiting this current room, in order to arrive in bedroom so that I could rest. I acknowledge that we did not use the Skype app to talk to eachother through means of wireless connection, but, I think that the fact that we did not do this does not particularly matter. I hope that the rest of your night continues well, I shall be with you again tommorow.',
-    'Have you ever been lying in bed unable to sleep and you try to clear your mind but by doing that youve inadvertently started consciously thinking about clearing your mind and that creates a perpetual cycle then you think about going to the bathroom because you need to but then you think about how cold it is outside and suddenly you dont need to as badly anymore and then you started to feel parched and then think about how far youd have to walk to get water and then you figure that youre not about to sleep anytime soon anyway so youre just like yknow what Ill go do whati need and when Im back Ill be tired enough to instantly sleep and so you do that and then you realize its pitch black but since your eyes alreadyadjusted you dont want to turn on the lights and you think you heard a noise and then you get really anxious and you run back to your bed but along the way you stub your toe on the textbook you accidentally left on the floor earlier and it burns like the fiery brimstones of hell and you jump upin down in silent agony since everyone else in the house is already asleep and then you get back in bed and try to ignore how dirty your feet gotduring the walk and you get really uncomfortable and then you start to thinking about the noise again and you start sweating and all of the suddenthe beds suddenly too hot and you flip the pillow and its like really cool and nice and you close your eyes and try to sleep but you still cant andthen you try to clear your mind but by doing that youve inadvertently started consciously thinking about clearing your mind and that creates a perpetualcycle where you do it all over again?',
-    'Goodnight future wife hope u have sweet dreams of our future where u fall in love w me pls when u wake up tell me how buff I got ... I wanna know ;) Im sure u will still b mrs pengting that u r ... but not as sexc as me. Oh yh also pls tell me what house we live in and is my pipi game gd in ur dreams. jokes Ik the answer to that I was just fishing for compliments (btw dont have wet dreams for it changing bedsheets must be a nightmare in the middle of the night) anyways hope u sleep well ... if ur dreaming of me in the future Ik u will sleep well cuz imma b HELLA rich and xtra as u will b too ;) hope u fell better in the morning hope u have a great day at school (pls try focusing in lessons, dont have to many flash forwards of my sexc self in the future no matter how hot I am. Dont stresss to much it will b alright in the end :) as u will see in ur dreams ;) dream guy ( sexc me in future) dream job (forensic phsychologist) and very wealthy w a big house and lots of love and loving if u know what I mean..... go to sleep soon and talk to me tmrw Frey goodnight'
+
+    # goodnights as if it were the 1500's
+    'Goodnight, fair maiden. May the morrow bring thee joy and mirth.',
+    'Goodnight, my lord. May the stars shine brightly upon thee.',
+    'Goodnight, kind sir. May the angels watch over thee as thou slumber.',
+    'Goodnight, fair lady. May the moonlight guide thee to sweet dreams.',
+    'Goodnight, noble knight. May the gods protect thee as thou rest.',
+    'Goodnight, gentlewoman. May the night be kind to thee.',
+    'Goodnight, good sir. May the heavens bless thee with peaceful sleep.',
+
+
+    # goodnights in the theme of toxic league players
+    'GG EZ, goodnight, noobs, get carried, noobs, get carried, noobs, get carried, noobs, get carried, noobs, get carried, noobs',
+    'CUZ YALL WERE TOO CHICKEN SHIT!!!!!!!!!!!!!!!!!!!! sleep well ijit'
 ]
 
 try:
-    with open('rare_goodnight.txt', 'r') as f:
-        rare_goodnight = f.readlines()
-
+    with open('rare_goodnight.txt', 'r') as f: rare_goodnight = f.readlines()
     RARE_GOODNIGHT_OPTIONS = [x.strip() for x in rare_goodnight]
 except Exception as e:
     RARE_GOODNIGHT_OPTIONS = ["Muah!", "Goodnight", "ay gn bud...", "xoxo <3"]
@@ -116,7 +106,7 @@ except Exception as e:
     sys.exit()
 
 RARE_GN_CHANCE_MIN = 0.0
-RARE_GN_CHANCE_MAX = 0.3
+RARE_GN_CHANCE_MAX = 0.5
 
 API_TOKEN = os.environ['API_TOKEN']
 
@@ -147,8 +137,7 @@ def pp(msg:str, sep:bool=False):
 
 def get_patch_notes():
     try:
-        with open('patch_notes.txt', 'r') as f:
-            patch_notes = f.readlines()
+        with open('patch_notes.txt', 'r') as f: patch_notes = f.readlines()
         return patch_notes
     except Exception as e:
         print(e)
@@ -156,7 +145,7 @@ def get_patch_notes():
 
 def is_rare_goodnight(): return random.random() < todays_rare_gn_chance
 
-def is_real_late_hour(): return datetime.now().hour in REAL_LATE_QUIPS
+def is_real_late_hour(): return datetime.now().hour in REAL_LATE_HOURS # IM FUCKING STUPID
 
 def is_goodnight_time(): return datetime.now().hour in GOODNIGHT_TIMES
 
